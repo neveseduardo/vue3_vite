e86f06b450aa756fb9d63e25ac174c5259a7570c
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2022-07-22 10:40:31 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus,
+        necessitatibus beatae? Architecto, inventore amet minima et vero culpa
+        provident soluta necessitatibus, perferendis, eveniet quam labore ab ad?
+        Eaque, explicabo dolore?
+    </div>
+</template>
+
+<script setup lang="ts"></script>
